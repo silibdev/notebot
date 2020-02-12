c18593c558bfbb4c277fe3e53aac9fc6ae375b21
Fabio Siliberto <fabio.siliberto@gmail.com>
2020-02-13 00:34:27 +0100

    Update task model and test usage

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,10 +1,12 @@
 from src.tasks.in_memory_task_manager import InMemoryTaskManager
-from src.tasks.task_model import TaskModel
+from src.tasks.task_model import TaskModel, Priority
 
 task_manager = InMemoryTaskManager()
 
 
 def main():
+    from datetime import datetime
+
     print('init')
     print(task_manager)
     print('')
@@ -14,13 +16,18 @@ def main():
     print(task_manager)
     print('')
 
-    add1.tags = ['tag1']
-    print('added tags to task 1')
+    add1.due_date = datetime.now()
+    print('added due date today to task 1')
     print(task_manager)
     print('')
 
     add2 = task_manager.add_task(TaskModel('Title 2', 'Desc 2'))
     print('added task 2')
+    print(task_manager)
+    print('')
+
+    add2.priority = Priority.HIGH
+    print('set priority high to task 2')
     print(task_manager)
     print('')
 
--- src/tasks/task_model.py
+++ src/tasks/task_model.py
@@ -1,15 +1,44 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+
+
+class Priority(Enum):
+    HIGH = 'high'
+    MEDIUM = 'medium'
+
+    def __str__(self):
+        return self.value
 
 
 class TaskModel:
     FORMAT = 'Id-Name-Description-Tags-Done'
 
-    def __init__(self, title, description='', tags=None):
+    def __init__(self, title, description='', priority: Priority=None, due_date: datetime = None):
         self.id = ''
         self.title = title
         self.description = description
-        self.tags = tags or []
+        self.priority = priority
+        self.due_date = due_date
         self.isDone = False
 
     def __str__(self):
-        return '{}-{}-{}-{}-{}'.format(self.id, self.title, self.description, self.tags, self.isDone)
+        task = '#{}'.format(self.id)
+
+        if self.priority:
+            task += ':{}:'.format(self.priority)
+        else:
+            task += ' '
+
+        task += '{}'.format(self.title)
+        task += '\n'
+
+        if self.description:
+            task += '{}\n'.format(self.description)
+
+        if self.due_date:
+            task += '{}\n'.format(self.due_date.strftime('%d-%b-%y %H:%M'))
+
+        if self.isDone:
+            task += 'DONE'
+
+        return task
